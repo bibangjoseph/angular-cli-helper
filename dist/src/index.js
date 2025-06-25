10ebee0ea989a168083abf6591ce9deb34e54ede
BIBANG BEFENE Joseph Donovan <bibangjose@gmail.com>
2025-06-25 20:18:01 +0100

    maj

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,5 +4,6 @@ export { createService } from './createService.js';
 export { createModel } from './createModel.js';
 export { createPackage } from './createPackage.js';
 export { createPage } from './createPage.js';
+export { initProject } from './initProject.js';
 
 // Ajouter d'autres fonctions pour les autres scripts
